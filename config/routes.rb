60ea7fe2845cc4f62c85829b0a95b38a8f30fe53
MarkWaldron <markewaldron@gmail.com>
2015-05-07 16:42:58 -0400

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :pins do
     member do
-      puts "like", to: "pins#upvote"
+      put "like", to: "pins#upvote"
     end
   end
 
